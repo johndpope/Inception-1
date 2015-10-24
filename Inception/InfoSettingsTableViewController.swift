1937d3402c8a8538269ec83ed4706d36e41ba5dc
David Ehlen <dehlen@me.com>
2015-10-24 23:13:43 +0200

    Change Email function from mailto url-scheme to MFMailComposerViewController in order to open the Mail in-app and don't switch to the mail app

--- Inception/InfoSettingsTableViewController.swift
+++ Inception/InfoSettingsTableViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class InfoSettingsTableViewController: UITableViewController {
+class InfoSettingsTableViewController: UITableViewController,MFMailComposeViewControllerDelegate {
     @IBOutlet weak var versionLabel:UILabel!
     
     override func viewDidLoad() {
@@ -26,10 +27,41 @@ class InfoSettingsTableViewController: UITableViewController {
     // MARK: - Table view delegate
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        if indexPath.row == 1 {
-            let email = "dehlen@me.com"
-            let url = NSURL(string: "mailto:\(email)")
-            UIApplication.sharedApplication().openURL(url!)
+        if indexPath.row == 2 {
+            let emailTitle = ""
+            let messageBody = self.generateMailBody()
+            let toRecipents = ["dehlen@me.com"]
+            let mc: MFMailComposeViewController = MFMailComposeViewController()
+            mc.mailComposeDelegate = self
+            mc.setSubject(emailTitle)
+            mc.setMessageBody(messageBody, isHTML: false)
+            mc.setToRecipients(toRecipents)
+            
+            self.presentViewController(mc, animated: true, completion: nil)
         }
+    }
+
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
+    func generateMailBody() -> String {
+        
+        let device = UIDevice.currentDevice()
+        let model = device.model
+        let systemVersion = device.systemVersion
+        let language = NSLocale.preferredLanguages()[0]
+        let country = NSLocale.currentLocale().localeIdentifier
+        let version = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String
+        let languageLocalized = "language".localized
+        let countryLocalized = "country".localized
+        
+        var returnString = "\n\n---\nModel: \(model)\nSystem Version: \(systemVersion)\n\(languageLocalized):\(language)\n\(countryLocalized): \(country)"
+        if version != nil {
+            returnString += "\nApp Version: \(version!)"
+        }
+        
+        return returnString
     }
 }
