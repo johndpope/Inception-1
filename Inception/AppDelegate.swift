8fb1de99d2a93086348d8631e7599b7cad5ba091
David Ehlen <dehlen@me.com>
2015-10-26 13:23:49 +0100

    removed print of core data model file url

--- Inception/AppDelegate.swift
+++ Inception/AppDelegate.swift
@@ -68,7 +68,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Watchlist.sqlite")
-        print(url)
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
             try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
