4d18ad31e36d7e9a64c7e48f4ab3a2b10311d9af
David Ehlen <dehlen@me.com>
2015-11-05 13:10:15 +0100

    refactored uitableviewdata extension with bindings

--- Inception/Movie+UITableViewData.swift
+++ Inception/Movie+UITableViewData.swift
@@ -13,86 +13,65 @@ extension Movie {
     func tableData() -> ([String],[String]) {
         var tableData:[String] = []
         var tableKeys:[String] = []
-        if self.voteAverage != nil {
-            if self.voteAverage! != 0.0 {
-                tableData.append("\(self.voteAverage!)")
-                tableKeys.append("voteAverage".localized)
-            }
+        
+        if let voteAverage = self.voteAverage where voteAverage != 0.0 {
+            tableData.append("\(voteAverage)")
+            tableKeys.append("voteAverage".localized)
         }
         
-        if self.status != nil {
-            if !self.status!.isEmpty {
-                tableData.append(self.status!)
-                tableKeys.append("status".localized)
-            }
+        if let status = self.status where !status.isEmpty {
+            tableData.append(status)
+            tableKeys.append("status".localized)
         }
         
-        if self.runtime != nil {
-            if self.runtime! != 0 {
-                tableData.append("\(self.runtime!)")
-                tableKeys.append("runtime".localized)
-            }
+        if let runtime = self.runtime where runtime != 0 {
+            tableData.append("\(runtime)")
+            tableKeys.append("runtime".localized)
         }
         
-        if self.releaseDate != nil {
-            if !self.releaseDate!.isEmpty {
-                let dateFormatter = NSDateFormatter()
-                dateFormatter.dateFormat = "yyyy-MM-dd"
-                let date:NSDate = dateFormatter.dateFromString(self.releaseDate!)!
-                dateFormatter.dateFormat = "dd.MM.yyyy"
-                
-                tableData.append(dateFormatter.stringFromDate(date))
-                tableKeys.append("releaseDate".localized)
-            }
+        if let releaseDate = self.releaseDate where !releaseDate.isEmpty {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let date:NSDate = dateFormatter.dateFromString(releaseDate)!
+            dateFormatter.dateFormat = "dd.MM.yyyy"
+            
+            tableData.append(dateFormatter.stringFromDate(date))
+            tableKeys.append("releaseDate".localized)
         }
         
-        if self.budget != nil {
-            if self.budget! != 0 {
-                tableData.append("\(self.budget!.addSeparator) $")
-                tableKeys.append("budget".localized)
-            }
+        if let budget = self.budget where budget != 0 {
+            tableData.append("\(budget.addSeparator) $")
+            tableKeys.append("budget".localized)
         }
         
-        if self.revenue != nil {
-            if self.revenue! != 0 {
-                tableData.append("\(self.revenue!.addSeparator) $")
-                tableKeys.append("revenue".localized)
-            }
+        if let revenue = self.revenue where revenue != 0 {
+            tableData.append("\(revenue.addSeparator) $")
+            tableKeys.append("revenue".localized)
         }
         
-        if self.tagline != nil {
-            if !self.tagline!.isEmpty {
-                tableData.append(self.tagline!)
-                tableKeys.append("tagline".localized)
-            }
+        if let tagline = self.tagline where !tagline.isEmpty {
+            tableData.append(tagline)
+            tableKeys.append("tagline".localized)
         }
         
-        if self.productionCompanies != nil {
-            if self.productionCompanies!.count != 0 {
-                tableData.append(self.productionCompanies!.joinWithSeparator(","))
-                tableKeys.append("productionCompanies".localized)
-            }
+        if let productionCompanies = self.productionCompanies where productionCompanies.count != 0 {
+            tableData.append(productionCompanies.joinWithSeparator(","))
+            tableKeys.append("productionCompanies".localized)
         }
         
-        if self.productionCountries != nil {
-            if self.productionCountries!.count != 0 {
-                tableData.append(self.productionCountries!.joinWithSeparator(","))
-                tableKeys.append("productionCountries".localized)
-            }
+        if let productionCountries = self.productionCountries where productionCountries.count != 0 {
+            tableData.append(productionCountries.joinWithSeparator(","))
+            tableKeys.append("productionCountries".localized)
         }
         
-        if self.genres != nil {
-            if !self.genres!.isEmpty {
-                tableData.append(self.genres!.map{$0.name!}.joinWithSeparator(", "))
-                tableKeys.append("genres".localized)
-            }
+        if let genres = self.genres where !genres.isEmpty {
+            tableData.append(genres.map{$0.name!}.joinWithSeparator(", "))
+            tableKeys.append("genres".localized)
         }
         
-        if self.overview != nil {
-            if !self.overview!.isEmpty {
-                tableData.append(self.overview!)
-                tableKeys.append("overview".localized)
-            }
+        if let overview = self.overview where !overview.isEmpty {
+            tableData.append(overview)
+            tableKeys.append("overview".localized)
         }
         return (tableKeys,tableData)
     }
--- Inception/Show+UITableViewData.swift
+++ Inception/Show+UITableViewData.swift
@@ -14,130 +14,99 @@ extension Show {
         var tableData:[String] = []
         var tableKeys:[String] = []
         
-        if self.voteAverage != nil {
-            if self.voteAverage! != 0.0 {
-                tableData.append("\(self.voteAverage!)")
-                tableKeys.append("voteAverage".localized)
-            }
+        if let voteAverage = self.voteAverage where voteAverage != 0.0 {
+            tableData.append("\(voteAverage)")
+            tableKeys.append("voteAverage".localized)
         }
         
-        if self.status != nil {
-            if !self.status!.isEmpty {
-                tableData.append(self.status!)
-                tableKeys.append("status".localized)
-            }
+        if let status = self.status where !status.isEmpty {
+            tableData.append(status)
+            tableKeys.append("status".localized)
         }
         
-        if self.numberOfEpisodes != nil {
-            if self.numberOfEpisodes! != 0 {
-                tableData.append("\(self.numberOfEpisodes!)")
-                tableKeys.append("numberOfEpisodes".localized)
-            }
+        if let numberOfEpisodes = self.numberOfEpisodes where numberOfEpisodes != 0 {
+            tableData.append("\(numberOfEpisodes)")
+            tableKeys.append("numberOfEpisodes".localized)
         }
         
-        if self.numberOfSeasons != nil {
-            if self.numberOfSeasons! != 0 {
-                tableData.append("\(self.numberOfSeasons!)")
-                tableKeys.append("numberOfSeasons".localized)
-            }
+        if let numberOfSeasons = self.numberOfSeasons where numberOfSeasons != 0 {
+            tableData.append("\(numberOfSeasons)")
+            tableKeys.append("numberOfSeasons".localized)
         }
         
-        if self.seasons != nil {
-            if self.seasons!.count > 0 {
-                tableData.append("showSeasons".localized)
-                tableKeys.append("showSeasons".localized)
-            }
+        if let seasons = self.seasons where seasons.count > 0 {
+            tableData.append("showSeasons".localized)
+            tableKeys.append("showSeasons".localized)
         }
         
-        if self.firstAirDate != nil {
-            if !self.firstAirDate!.isEmpty {
-                let dateFormatter = NSDateFormatter()
-                dateFormatter.dateFormat = "yyyy-MM-dd"
-                let date:NSDate = dateFormatter.dateFromString(self.firstAirDate!)!
-                dateFormatter.dateFormat = "dd.MM.yyyy"
-                
-                tableData.append(dateFormatter.stringFromDate(date))
-                tableKeys.append("firstAirDate".localized)
-            }
+        if let firstAirDate = self.firstAirDate where !firstAirDate.isEmpty {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let date:NSDate = dateFormatter.dateFromString(firstAirDate)!
+            dateFormatter.dateFormat = "dd.MM.yyyy"
+            
+            tableData.append(dateFormatter.stringFromDate(date))
+            tableKeys.append("firstAirDate".localized)
         }
         
-        if self.lastAirDate != nil {
-            if !self.lastAirDate!.isEmpty {
-                let dateFormatter = NSDateFormatter()
-                dateFormatter.dateFormat = "yyyy-MM-dd"
-                let date:NSDate = dateFormatter.dateFromString(self.lastAirDate!)!
-                dateFormatter.dateFormat = "dd.MM.yyyy"
-                
-                tableData.append(dateFormatter.stringFromDate(date))
-                tableKeys.append("lastAirDate".localized)
-            }
+        if let lastAirDate = self.lastAirDate where !lastAirDate.isEmpty {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let date:NSDate = dateFormatter.dateFromString(lastAirDate)!
+            dateFormatter.dateFormat = "dd.MM.yyyy"
+            
+            tableData.append(dateFormatter.stringFromDate(date))
+            tableKeys.append("lastAirDate".localized)
         }
         
         
-        if self.episodeRunTime != nil {
-            if self.episodeRunTime!.count != 0 {
-                tableData.append(self.episodeRunTime!.map{String($0)}.joinWithSeparator(", "))
-                tableKeys.append("episodeRuntime".localized)
-            }
+        if let episodeRunTime = self.episodeRunTime where episodeRunTime.count != 0 {
+            tableData.append(episodeRunTime.map{String($0)}.joinWithSeparator(", "))
+            tableKeys.append("episodeRuntime".localized)
         }
         
-        if self.inProduction != nil {
+        if let inProduction = self.inProduction {
             var string = "no".localized;
-            if self.inProduction! {
+            if inProduction {
                 string = "yes".localized
             }
-            
             tableData.append(string)
             tableKeys.append("inProduction".localized)
         }
         
-        if self.createdBy != nil {
-            if self.createdBy!.count != 0 {
-                tableData.append(self.createdBy!.joinWithSeparator(","))
-                tableKeys.append("createdBy".localized)
-            }
+        if let createdBy = self.createdBy where createdBy.count != 0 {
+            tableData.append(createdBy.joinWithSeparator(","))
+            tableKeys.append("createdBy".localized)
         }
         
-        if self.networks != nil {
-            if self.networks!.count != 0 {
-                tableData.append(self.networks!.joinWithSeparator(","))
-                tableKeys.append("networks".localized)
-            }
+        if let networks = self.networks where networks.count != 0 {
+            tableData.append(networks.joinWithSeparator(","))
+            tableKeys.append("networks".localized)
         }
         
-        if self.originCountries != nil {
-            if self.originCountries!.count != 0 {
-                tableData.append(self.originCountries!.joinWithSeparator(","))
-                tableKeys.append("originCountries".localized)
-            }
+        if let originCountries = self.originCountries where originCountries.count != 0 {
+            tableData.append(originCountries.joinWithSeparator(","))
+            tableKeys.append("originCountries".localized)
         }
         
-        if self.type != nil {
-            if !self.type!.isEmpty {
-                tableData.append(self.type!)
-                tableKeys.append("type".localized)
-            }
+        if let type = self.type where !type.isEmpty  {
+            tableData.append(type)
+            tableKeys.append("type".localized)
         }
         
-        if self.productionCompanies != nil {
-            if self.productionCompanies!.count != 0 {
-                tableData.append(self.productionCompanies!.joinWithSeparator(","))
-                tableKeys.append("productionCompanies".localized)
-            }
+        if let productionCompanies = self.productionCompanies where productionCompanies.count != 0 {
+            tableData.append(productionCompanies.joinWithSeparator(","))
+            tableKeys.append("productionCompanies".localized)
         }
         
-        if self.genres != nil {
-            if !self.genres!.isEmpty {
-                tableData.append(self.genres!.map{$0.name!}.joinWithSeparator(", "))
-                tableKeys.append("genres".localized)
-            }
+        if let genres = self.genres where !genres.isEmpty {
+            tableData.append(genres.map{$0.name!}.joinWithSeparator(", "))
+            tableKeys.append("genres".localized)
         }
         
-        if self.overview != nil {
-            if !self.overview!.isEmpty {
-                tableData.append(self.overview!)
-                tableKeys.append("overview".localized)
-            }
+        if let overview = self.overview where !overview.isEmpty {
+            tableData.append(overview)
+            tableKeys.append("overview".localized)
         }
         
         return(tableKeys,tableData)
